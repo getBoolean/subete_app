0fdb4e1548e0bef92a6c80da469658a0c1558be6
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-02-18 20:45:13 -0600

    Move class to separate file

--- packages/pub/kavita_api/lib/kavita_api.dart
+++ packages/pub/kavita_api/lib/kavita_api.dart
@@ -2,4 +2,5 @@
 library;
 
 export 'src/kavita_api.dart';
+export 'src/kavita_api_authenticator.dart';
 export 'src/openapi_generated_code/kavita_api.swagger.dart' hide KavitaApi;
--- packages/pub/kavita_api/lib/src/kavita_api.dart
+++ packages/pub/kavita_api/lib/src/kavita_api.dart
@@ -1,40 +1,7 @@
 import 'package:chopper/chopper.dart';
+import 'package:kavita_api/src/kavita_api_authenticator.dart';
 import 'package:kavita_api/src/openapi_generated_code/kavita_api.swagger.dart'
     as client;
-
-/// Authenticates with Kavita API
-final class KavitaApiAuthenticator {
-  final Uri baseUrl;
-  final String? username;
-  final String? password;
-  final String? apiKey;
-  final client.KavitaApi _api;
-
-  /// Authenticates with Kavita API
-  KavitaApiAuthenticator({
-    required this.baseUrl,
-    this.username,
-    this.password,
-  })  : _api = client.KavitaApi.create(baseUrl: baseUrl),
-        apiKey = null;
-
-  KavitaApiAuthenticator.apiKey(
-    this.apiKey, {
-    required this.baseUrl,
-  })  : _api = client.KavitaApi.create(baseUrl: baseUrl),
-        username = null,
-        password = null;
-
-  Future<Response<client.UserDto>> login() async {
-    return await _api.apiAccountLoginPost(
-      body: client.LoginDto(
-        username: username,
-        password: password,
-        apiKey: apiKey,
-      ),
-    );
-  }
-}
 
 final class KavitaApi {
   final Uri baseUrl;
