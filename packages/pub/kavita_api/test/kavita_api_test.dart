ed7bb1afc5dc96d6ff54379856fdc9faedb8e035
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-02-18 20:04:59 -0600

    Add kavita experimental tests

--- packages/pub/kavita_api/test/kavita_api_test.dart
+++ packages/pub/kavita_api/test/kavita_api_test.dart
@@ -1,11 +1,46 @@
+import 'package:dotenv/dotenv.dart';
+import 'package:kavita_api/kavita_api.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
+  late final KavitaApi client;
+
+  setUpAll(() async {
+    final env = DotEnv()..load();
+    if (!env.isEveryDefined(['PASSWORD', 'USERNAME'])) {
+      throw Exception(
+        'Please define the environment variables `PASSWORD` and `USERNAME` in an .env file.',
+      );
+    }
+
+    final baseUrl =
+        Uri.parse(env.getOrElse('BASE_URL', () => 'http://127.0.0.1:5000'));
+    final accountAuthenticator = KavitaApiAuthenticator(
+      username: env['USERNAME'],
+      password: env['PASSWORD'],
+      baseUrl: baseUrl,
+    );
+    final userResponse = await accountAuthenticator.login();
+    if (userResponse.body == null || userResponse.body!.token == null) {
+      throw Exception('Could not log in');
+    }
+
+    print('Logged in as: ${userResponse.body!.username}');
+
+    client = KavitaApi(
+      baseUrl: baseUrl,
+      bearerToken: userResponse.body!.token!,
+    );
+  });
+
+  group('Test Kavita API', () {
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {});
+    test('Test Server Info', () async {
+      final res = await client.server.getServerInfo();
+      expect(res.statusCode, 200, reason: 'Could not connect to server');
+    });
   });
 }
