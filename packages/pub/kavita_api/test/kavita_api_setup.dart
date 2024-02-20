64548da7d1431bb7f70c519258713b838b82f801
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-02-19 20:27:06 -0600

    Split test file

---
+++ packages/pub/kavita_api/test/kavita_api_setup.dart
@@ -0,0 +1,21 @@
+import 'package:dotenv/dotenv.dart';
+import 'package:kavita_api/kavita_api.dart';
+
+Future<KavitaApi> setUpKavita() async {
+  final env = DotEnv(includePlatformEnvironment: true, quiet: true)..load();
+  if (!env.isEveryDefined(['KAVITA_PASSWORD', 'KAVITA_USERNAME'])) {
+    throw Exception(
+      'Please define the environment variables `KAVITA_PASSWORD` and `KAVITA_USERNAME` in an .env file.',
+    );
+  }
+
+  final baseUrl = Uri.parse(
+      env.getOrElse('KAVITA_BASE_URL', () => 'http://127.0.0.1:5000'));
+  final client = KavitaApi(baseUrl: baseUrl);
+  await client.v1.account.login(
+    username: env['KAVITA_USERNAME']!,
+    password: env['KAVITA_PASSWORD']!,
+  );
+
+  return client;
+}
--- packages/pub/kavita_api/test/v1/download_test.dart
+++ packages/pub/kavita_api/test/v1/download_test.dart
@@ -1,17 +1,18 @@
 import 'package:kavita_api/kavita_api.dart';
 import 'package:test/test.dart';
 
-import 'kavita_api_setup.dart';
+import '../kavita_api_setup.dart';
 
 void main() {
   late final KavitaApi client;
 
   setUpAll(() async => client = await setUpKavita());
 
-  group('Test Kavita API v1 Server', () {
-    test('Test Server Info', () async {
-      final res = await client.v1.server.getServerInfo();
+  group('Test Kavita API v1 Download', () {
+    test('Test Download Volume', () async {
+      final res = await client.v1.download.downloadVolume(volumeId: 22800);
       expect(res.statusCode, 200, reason: 'Could not connect to server');
+      expect(res.body, isNotEmpty, reason: 'No data received');
     });
   });
 }
