38abf679efc2a8fc2bfaae9e51f01bbe95b49b1c
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-02-22 03:17:31 -0600

    remove type casting with const constructor

--- packages/pub/kavita_api/lib/src/service/entities/device_platform.dart
+++ packages/pub/kavita_api/lib/src/service/entities/device_platform.dart
@@ -1,6 +1,6 @@
-extension type DevicePlatform(int value) implements int {
-  static const DevicePlatform custom = 0 as DevicePlatform;
-  static const DevicePlatform pocketBook = 1 as DevicePlatform;
-  static const DevicePlatform kindle = 2 as DevicePlatform;
-  static const DevicePlatform kobo = 3 as DevicePlatform;
+extension type const DevicePlatform(int value) implements int {
+  static const DevicePlatform custom = DevicePlatform(0);
+  static const DevicePlatform pocketBook = DevicePlatform(1);
+  static const DevicePlatform kindle = DevicePlatform(2);
+  static const DevicePlatform kobo = DevicePlatform(3);
 }
