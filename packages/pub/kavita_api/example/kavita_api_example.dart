859c108132efe413630193a565eb64f5f25022e9
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-03-03 01:02:22 -0600

    Add example file to kavita

--- packages/pub/kavita_api/example/kavita_api_example.dart
+++ packages/pub/kavita_api/example/kavita_api_example.dart
@@ -1,3 +1,22 @@
-void main() {
-  print('awesome');
+import 'package:kavita_api/kavita_api.dart';
+
+void main() async {
+  final baseUrl = Uri.parse('http://localhost:5000');
+  final api = KavitaApi(baseUrl: baseUrl);
+
+  // Option 1:
+  await api.account.login(
+    username: 'Username',
+    password: 'Password',
+  );
+
+  // Option 2:
+  await api.plugin.authenticate(
+    apiKey: 'API_KEY_FOR_USER_HERE',
+    pluginName: 'YOUR_APP_NAME_HERE',
+  );
+
+  // Now we can use methods that require authentication
+  final userResponse = await api.account.refreshAccount();
+  print(userResponse.body?.username);
 }
