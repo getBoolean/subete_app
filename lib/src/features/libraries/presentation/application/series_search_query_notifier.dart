98b535d31a28269d33ad4df2ce19f6d8644f3f51
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-04-13 22:26:25 -0500

    add search query notifier

---
+++ lib/src/features/libraries/presentation/application/series_search_query_notifier.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:log/log.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'series_search_query_notifier.g.dart';
+
+@riverpod
+class SeriesSearchQueryNotifier extends _$SeriesSearchQueryNotifier {
+  final log = Logger('MoviesSearchQueryNotifier');
+  final _searchQueryController = StreamController<String>.broadcast();
+  Timer? _debounceTimer;
+  late final StreamSubscription<String> _subscription;
+
+  @override
+  String build() {
+    _subscription = _searchQueryController.stream.listen((query) {
+      // Cancel existing timer if there is one
+      _debounceTimer?.cancel();
+      // Set a new timer to debounce the query
+      _debounceTimer = Timer(const Duration(milliseconds: 500), () {
+        _updateState(query);
+      });
+    });
+
+    // close the StreamController and cancel the subscriptions on dispose
+    ref.onDispose(() {
+      _searchQueryController.close();
+      _subscription.cancel();
+      _debounceTimer?.cancel();
+    });
+    return '';
+  }
+
+  void _updateState(String query) {
+    state = query;
+    log.fine('debounce state: $query');
+  }
+
+  void setQuery(String query) {
+    _searchQueryController.sink.add(query);
+    log.fine('debounce query: $query');
+  }
+}
---
+++ lib/src/features/libraries/presentation/application/series_search_query_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'series_search_query_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$seriesSearchQueryNotifierHash() =>
+    r'60cddc478107c8f56b90d23e87fac4643a35e8bf';
+
+/// See also [SeriesSearchQueryNotifier].
+@ProviderFor(SeriesSearchQueryNotifier)
+final seriesSearchQueryNotifierProvider =
+    AutoDisposeNotifierProvider<SeriesSearchQueryNotifier, String>.internal(
+  SeriesSearchQueryNotifier.new,
+  name: r'seriesSearchQueryNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$seriesSearchQueryNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SeriesSearchQueryNotifier = AutoDisposeNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
