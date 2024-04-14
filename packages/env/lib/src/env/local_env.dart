19fc64964e30de35c85e7dcb6dfb458f31a15e30
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-04-13 21:40:21 -0500

    add Kavita demo as default,, API Key overrides

--- packages/env/lib/src/env/local_env.dart
+++ packages/env/lib/src/env/local_env.dart
@@ -17,8 +17,7 @@ class LocalEnv implements EnvFlavor, EnvFields {
   final bool usePathUrlStrategy = _Env.usePathUrlStrategy;
 
   @override
-  @EnviedField(
-      varName: 'KAVITA_BASE_URL', defaultValue: 'http://127.0.0.1:5000')
+  @EnviedField(varName: 'KAVITA_BASE_URL', defaultValue: kKavitaBaseUrlDemo)
   final String kavitaBaseUrl = _Env.kavitaBaseUrl;
 
   @override
@@ -31,10 +30,16 @@ class LocalEnv implements EnvFlavor, EnvFields {
   final String? kavitaApiKey = _Env.kavitaApiKey;
 
   @override
-  @EnviedField(varName: 'KAVITA_USERNAME', optional: true)
-  final String? kavitaUsername = _Env.kavitaUsername;
+  @EnviedField(
+      varName: 'KAVITA_USERNAME',
+      defaultValue: kKavitaUsernameDemo,
+      obfuscate: true)
+  final String kavitaUsername = _Env.kavitaUsername;
 
   @override
-  @EnviedField(varName: 'KAVITA_PASSWORD', optional: true, obfuscate: true)
-  final String? kavitaPassword = _Env.kavitaPassword;
+  @EnviedField(
+      varName: 'KAVITA_PASSWORD',
+      defaultValue: kKavitaPasswordDemo,
+      obfuscate: true)
+  final String kavitaPassword = _Env.kavitaPassword;
 }
