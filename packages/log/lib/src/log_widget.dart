5b94eae7ecf33955dc81a0a9122e417d37637e0a
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-06-01 01:27:21 -0500

    remove outdated logger_flutter_plus

--- packages/log/lib/src/log_widget.dart
+++ packages/log/lib/src/log_widget.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:log/src/log_config.dart';
-import 'package:logger_flutter_plus/logger_flutter_plus.dart';
 import 'package:logging/logging.dart';
 
 /// A widget that initializes logging for the app.
@@ -18,50 +16,16 @@ class LoggerWidget extends StatefulWidget {
 }
 
 class _LoggerWidgetState extends State<LoggerWidget> {
-  late final LogConfig logConfig;
   bool isShowingConsole = false;
 
   @override
   void initState() {
-    logConfig = LogConfig(widget.format);
+    LogConfig.init(widget.format);
     super.initState();
   }
 
   @override
-  void dispose() {
-    logConfig.close();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (kProfileMode) {
-      return ShakeDetectorWidget(
-        shakeDetector: DefaultShakeDetector(
-          onPhoneShake: () async {
-            if (isShowingConsole) {
-              return;
-            }
-            isShowingConsole = true;
-            final theme = Theme.of(context);
-            await Navigator.push(
-              context,
-              MaterialPageRoute<dynamic>(
-                builder: (context) => Scaffold(
-                  body: LogConsoleWidget(
-                    showCloseButton: true,
-                    theme: LogConsoleTheme.byTheme(theme),
-                    logConsoleManager: logConfig.consoleManager,
-                  ),
-                ),
-              ),
-            );
-            isShowingConsole = false;
-          },
-        ),
-        child: widget.child,
-      );
-    }
     return widget.child;
   }
 }
