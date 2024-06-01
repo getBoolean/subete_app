5318709fa6995f086fcf767980cc90e89601171f
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-06-01 19:57:24 +0000

    Deploying to gh-pages from @ getBoolean/subete_app@59f4672931ba8c452ec218e35962be56b2f126e6 🚀

--- feature/draggable/flutter_bootstrap.js
+++ feature/draggable/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "124513356"
+    serviceWorkerVersion: "2417723367"
   }
 });
